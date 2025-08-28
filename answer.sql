5f213923a64bd0b48470b0b27308c734715b9266
Biruktawit <btesfaye970@gmail.com>
2025-08-28 22:37:49 +0300

    Add files via upload

---
+++ answer.sql
@@ -0,0 +1,8 @@
+-- Assignment: Introduction to Databases
+-- Student: Biruktawit Tesfaye
+
+-- Question 1: Create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- Question 2: Drop (delete) the database called demo
+DROP DATABASE demo;
